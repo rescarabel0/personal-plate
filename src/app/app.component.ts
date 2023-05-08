e6ca152993fcb5fdcbfb2938f9cab5c3afd2eea0
rescarabel0 <r.escarabelo@gmail.com>
2023-05-08 02:13:44 -0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AppService} from "./app.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'personal-plate';
+export class AppComponent implements OnInit {
+  form: FormGroup
+
+  private sub
+
+  constructor(private fb: FormBuilder, private appService: AppService, private route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      login: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+    this.route.queryParams.subscribe(q => {
+      console.log(q)
+    })
+  }
+
+
+
+  submit(): void {
+    console.log(this.form)
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {ReactiveFormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([]),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
